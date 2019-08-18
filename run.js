b6a98d8541838e7ded32fdfebb44e2e70d3f4f1d
atxiu <33857802+atxiu@users.noreply.github.com>
2019-08-18 08:58:29 +0800

    Update run.js

--- run.js
+++ run.js
@@ -20,23 +20,46 @@ if(runParams.indexOf(':') >= 0) {
   method = runParams.split(':')[1];
 }
 let shadowsocks;
-if(runParams.indexOf('python') >= 0) {
-  type = 'python';
-  const tempPassword = 'qwerASDF' + Math.random().toString().substr(2, 8);
-  shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '--manager-address', ssConfig ]);
-} else {
-  shadowsocks = spawn('ss-manager', [ '-m', method, '-u', '--manager-address', ssConfig ]);
+// 检测端口是否已占用，占用则端口号-1
+const net=require('net')
+function portIsOccupied (port){
+    const server=net.createServer().listen(port)
+    return new Promise((resolve,reject)=>{
+        server.on('listening',()=>{
+            server.close()
+            resolve(port)
+        })
+        server.on('error',(err)=>{
+            if(err.code==='EADDRINUSE'){
+                resolve(portIsOccupied(port-1)) // 注意这句，如占用端口号-1
+            }else{
+                reject(err)
+            }
+        })
+    })
 }
+// 默认端口65535
+portIsOccupied(65535)
+.then(port=>{
+    // console.log(port);
+    if(runParams.indexOf('python') >= 0) {
+    type = 'python';
+    const tempPassword = 'qwerASDF' + Math.random().toString().substr(2, 8);
+        shadowsocks = spawn('ssserver', ['-m', method, '-p', port, '-k', tempPassword, '--manager-address', ssConfig ]);
+    } else {
+    shadowsocks = spawn('ss-manager', [ '-m', method, '-u', '--manager-address', ssConfig ]);
+    }
 
-shadowsocks.stdout.on('data', (data) => {
-  // console.log(`stdout: ${data}`);
-});
+    shadowsocks.stdout.on('data', (data) => {
+    // console.log(`stdout: ${data}`);
+    });
 
-shadowsocks.stderr.on('data', (data) => {
-  // console.error(`stderr: ${data}`);
-});
+    shadowsocks.stderr.on('data', (data) => {
+    // console.error(`stderr: ${data}`);
+    });
 
-shadowsocks.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
-console.log(`Run shadowsocks (${ type === 'python' ? 'python' : 'libev'})`);
+    shadowsocks.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    });
+    console.log(`Run shadowsocks (${ type === 'python' ? 'python' : 'libev'})`);
+})
